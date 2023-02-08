8d1e62a0b3cdeb5d49638366e838df3e25d89362
Daniil Vinogradov <xitrix@bk.ru>
2023-02-08 15:28:39 +0100

    Initial Commit

---
+++ SDLTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDLTest
+//
+//  Created by Даниил Виноградов on 08.02.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
