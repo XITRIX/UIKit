08c925670ee890de82e74c5f4ded33fccee493ef
Daniil Vinogradov <xitrix@bk.ru>
2023-03-15 14:10:52 +0100

    DispatchQueue global dispatch implemented

--- SDLTest/DispatchQueue/DispatchQueue.cpp
+++ SDLTest/DispatchQueue/DispatchQueue.cpp
@@ -6,31 +6,60 @@
 //
 
 #include <DispatchQueue/DispatchQueue.hpp>
+#include <libretro-common/retro_timers.h>
 
 namespace UIKit {
 
 DispatchQueue* DispatchQueue::_main = nullptr;
+DispatchQueue* DispatchQueue::_global = nullptr;
 
 DispatchQueue* DispatchQueue::main() {
     if (!_main) _main = new DispatchQueue("main");
     return _main;
 }
 
-DispatchQueue::DispatchQueue(std::string tag): _tag(tag) { }
+DispatchQueue* DispatchQueue::global() {
+    if (!_global) _global = new DispatchQueue("global");
+    return _global;
+}
+
+DispatchQueue::DispatchQueue(std::string tag): _tag(tag) {
+    if (tag != "main") {
+        pthread_create(&_task_loop_thread, NULL, task_loop, this);
+    }
+}
+
+DispatchQueue::~DispatchQueue() {
+    _task_loop_active = false;
+    pthread_join(_task_loop_thread, NULL);
+}
 
 void DispatchQueue::async(std::function<void()> task) {
+    std::lock_guard<std::mutex> guard(_m_async_mutex);
     _queue.push(task);
 }
 
 void DispatchQueue::performAll() {
     std::queue<std::function<void()>> copy;
-    std::swap( _queue, copy );
+    {
+        std::lock_guard<std::mutex> guard(_m_async_mutex);
+        std::swap( _queue, copy );
+    }
     
     while (!copy.empty()) {
         auto task = copy.front();
         copy.pop();
         task();
     }
+}
+
+void* DispatchQueue::task_loop(void* a) {
+    auto self = (DispatchQueue*) a;
+    while (self->_task_loop_active) {
+        self->performAll();
+        retro_sleep(500);
+    }
+    return NULL;
 }
 
 }
--- SDLTest/DispatchQueue/DispatchQueue.hpp
+++ SDLTest/DispatchQueue/DispatchQueue.hpp
@@ -10,6 +10,8 @@
 #include <queue>
 #include <string>
 #include <functional>
+#include <thread>
+#include <mutex>
 
 #ifdef main
 #undef main
@@ -20,14 +22,23 @@ namespace UIKit {
 class DispatchQueue {
 public:
     DispatchQueue(std::string tag);
+    ~DispatchQueue();
+
     static DispatchQueue* main();
+    static DispatchQueue* global();
 
     std::string tag() const { return _tag; }
     void async(std::function<void()> task);
 private:
     static DispatchQueue* _main;
+    static DispatchQueue* _global;
     std::queue<std::function<void()>> _queue;
     std::string _tag;
+
+    pthread_t _task_loop_thread;
+    std::mutex _m_async_mutex;
+    volatile bool _task_loop_active = true;
+    static void* task_loop(void* a);
 
     void performAll();
 
